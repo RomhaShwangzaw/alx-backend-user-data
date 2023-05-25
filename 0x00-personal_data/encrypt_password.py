1d78eef4931c29033c4f080a35dff46e4d4e8990
Romha Shwangzaw <romhas@gmail.com>
2023-05-25 16:05:22 +0300

    added function that validates if a password and its hashed counterpart match

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,14 @@ def hash_password(password: str) -> bytes:
     pwd = bytes(password, "utf-8")
     hashed = bcrypt.hashpw(pwd, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if the provided password matches the hashed password
+    Return:
+    `True` if the passwords match
+    `False` if the passwords don't match
+    """
+    pwd = bytes(password, "utf-8")
+    return bcrypt.checkpw(pwd, hashed_password)
