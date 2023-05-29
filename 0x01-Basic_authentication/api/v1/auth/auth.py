e7dc2f4678395f029b680c0be90d5e86841c9512
Romha Shwangzaw <romhas@gmail.com>
2023-05-29 13:13:29 +0300

    added implementation of require_auth() method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,9 +9,22 @@ class Auth:
     """ Authentication Class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method that authenticates users
+        """ Method that defines which routes don't need authentication
+        Return:
+          - True if `path` is None
+          - True if `excluded_paths` is None or empty
+          - False if `path` is in `excluded_paths`
+        Assumptions:
+          - `excluded_paths` contains string path always ending by a /
+          - method is slash tolerant: path=/api/v1/status and
+            path=/api/v1/status/ must be returned False if
+            `excluded_paths` contains /api/v1/status/
         """
-        return False
+        if not path or not excluded_paths or len(excluded_paths) == 0:
+            return True
+        if any(path in x for x in excluded_paths):
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method that adds authorization credentials to request header
