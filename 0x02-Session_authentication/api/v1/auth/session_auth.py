b18a96ad0f29174b2f6409b186d8498b5eedb48d
Romha Shwangzaw <romhas@gmail.com>
2023-06-01 12:06:25 +0300

    overloaded the current_user() method in SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ Module of Session Authentication
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -34,3 +35,10 @@ class SessionAuth(Auth):
         if not session_id or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns a User instance based on a cookie value
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
