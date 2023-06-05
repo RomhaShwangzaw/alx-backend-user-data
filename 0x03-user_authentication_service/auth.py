c767c9e0097b56d3fd4d307619192d02a3d3ba06
Romha Shwangzaw <romhas@gmail.com>
2023-06-05 18:01:16 +0300

    added get_user_from_session_id() method to the Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,3 +57,13 @@ class Auth:
             return session_id
         except Exception:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ Retrieves a User based on his/her Session ID
+        """
+        if not session_id:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except Exception:
+            return None
