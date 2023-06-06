786bfe3a8d9437302dca81068b64c91acf2f962c
Romha Shwangzaw <romhas@gmail.com>
2023-06-06 21:17:08 +0300

    Added POST /reset_password route to the Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,16 +8,20 @@ AUTH = Auth()
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
-    """ Returns a welcome message
+    """ GET /
+    Return:
+      - welcome message
     """
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route("/users", methods=["POST"])
+@app.route("/users", methods=["POST"], strict_slashes=False)
 def users() -> str:
-    """ Registers a new user
+    """ POST /users
+    Return:
+      - success message
     """
     email = request.form.get("email")
     password = request.form.get("password")
@@ -28,9 +32,11 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route("/sessions", methods=["POST"])
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login() -> str:
-    """ Creates a new session for the user
+    """ POST /sessions
+    Return:
+      - success message
     """
     email = request.form.get("email")
     password = request.form.get("password")
@@ -42,9 +48,11 @@ def login() -> str:
     return response
 
 
-@app.route("/sessions", methods=["DELETE"])
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout() -> str:
-    """ Deletes a user Session
+    """ DELETE /sessions
+    Return:
+      - redirection page
     """
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
@@ -55,15 +63,31 @@ def logout() -> str:
         abort(403)
 
 
-@app.route("/profile")
+@app.route("/profile", methods=["GET"], strict_slashes=False)
 def profile() -> str:
-    """ Retrieves a user based on Session ID cookie
+    """ GET /profile
+    Return:
+      - success message
     """
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         return jsonify({"email": user.email})
     abort(403)
+
+
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """ POST /reset_password
+    Return:
+      - success message
+    """
+    email = request.form.get("email")
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
